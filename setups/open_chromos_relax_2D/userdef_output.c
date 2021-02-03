3c1723e265c2d2acc711dcce25472aef7f0197c2
Gabriel Pelouze <gabriel@pelouze.net>
2021-02-03 15:37:28 +0100

    add visc nu1 to userdef output in relax 2D setup

--- setups/open_chromos_relax_2D/userdef_output.c
+++ setups/open_chromos_relax_2D/userdef_output.c
@@ -16,12 +16,14 @@ void ComputeUserVar (const Data *d, Grid *grid)
   int i, j, k, nv;
   double mu;
   double *v;
-  double ***T, ***divB;
+  double ***T, ***divB, ***nu1;
+  double nu1_buff, nu2_buff;
 
   v = ARRAY_1D(NVAR, double);
 
   T = GetUserVar("T");
   divB = GetUserVar("divB");
+  nu1 = GetUserVar("nu1");
 
   DOM_LOOP(k,j,i){
     NVAR_LOOP(nv) {
@@ -34,6 +36,8 @@ void ComputeUserVar (const Data *d, Grid *grid)
       + (d->Vc[BX2][k][j+1][i] - d->Vc[BX2][k][j-1][i]) / grid->dx[JDIR][j],
       + (d->Vc[BX3][k+1][j][i] - d->Vc[BX2][k-1][j][i]) / grid->dx[KDIR][k]
       ));
+    Visc_nu(v, grid->x[IDIR][i], grid->x[JDIR][j], grid->x[KDIR][k], &nu1_buff, &nu2_buff);
+    nu1[k][j][i] = nu1_buff;
   }
 
 }
