ce1cc08066f5805bf01d9ecfc0f6da625ded751e
Gabriel Pelouze <gabriel@pelouze.net>
2021-02-12 09:54:36 +0100

    change 2D/definitions.h indentation

--- setups/open_chromos_relax_2D/definitions.h
+++ setups/open_chromos_relax_2D/definitions.h
@@ -1,28 +1,28 @@
-#define  PHYSICS                 MHD
-#define  DIMENSIONS              2
-#define  COMPONENTS              2
-#define  GEOMETRY                CARTESIAN
-#define  BODY_FORCE              VECTOR
-#define  FORCED_TURB             NO
-#define  COOLING                 NO
-#define  RECONSTRUCTION          LINEAR
-#define  TIME_STEPPING           CHARACTERISTIC_TRACING
-#define  DIMENSIONAL_SPLITTING   NO
-#define  NTRACER                 0
-#define  USER_DEF_PARAMETERS     13
+#define  PHYSICS                        MHD
+#define  DIMENSIONS                     2
+#define  COMPONENTS                     2
+#define  GEOMETRY                       CARTESIAN
+#define  BODY_FORCE                     VECTOR
+#define  FORCED_TURB                    NO
+#define  COOLING                        NO
+#define  RECONSTRUCTION                 LINEAR
+#define  TIME_STEPPING                  CHARACTERISTIC_TRACING
+#define  DIMENSIONAL_SPLITTING          NO
+#define  NTRACER                        0
+#define  USER_DEF_PARAMETERS            13
 
 /* -- physics dependent declarations -- */
 
-#define  EOS                     IDEAL
-#define  ENTROPY_SWITCH          NO
-#define  DIVB_CONTROL            DIV_CLEANING
-#define  BACKGROUND_FIELD        NO
-#define  AMBIPOLAR_DIFFUSION     NO
-#define  RESISTIVITY             NO
-#define  HALL_MHD                NO
-#define  THERMAL_CONDUCTION      SUPER_TIME_STEPPING
-#define  VISCOSITY               SUPER_TIME_STEPPING
-#define  ROTATING_FRAME          NO
+#define  EOS                            IDEAL
+#define  ENTROPY_SWITCH                 NO
+#define  DIVB_CONTROL                   DIV_CLEANING
+#define  BACKGROUND_FIELD               NO
+#define  AMBIPOLAR_DIFFUSION            NO
+#define  RESISTIVITY                    NO
+#define  HALL_MHD                       NO
+#define  THERMAL_CONDUCTION             SUPER_TIME_STEPPING
+#define  VISCOSITY                      SUPER_TIME_STEPPING
+#define  ROTATING_FRAME                 NO
 
 /* -- user-defined parameters (labels) -- */
 
@@ -42,19 +42,18 @@
 
 /* [Beg] user-defined constants (do not change this line) */
 
-#define  UNIT_LENGTH             1e8
-#define  UNIT_VELOCITY           sqrt(CONST_kB/(0.5*CONST_mp)*1e6)
-#define  UNIT_DENSITY            1e-15
-
-#define  VTK_TIME_INFO           YES
-#define  GLM_EXTENDED            YES
-#define  INITIAL_SMOOTHING       YES
-#define  CHAR_LIMITING           YES
-#define  LIMITER                 MC_LIM
-#define  PRINT_TO_FILE           NO
-#define  H_MASS_FRAC             1.0
-#define  He_MASS_FRAC            0.0
-#define  INTERNAL_BOUNDARY       YES
+#define  UNIT_LENGTH                    1e8
+#define  UNIT_VELOCITY                  sqrt(CONST_kB/(0.5*CONST_mp)*1e6)
+#define  UNIT_DENSITY                   1e-15
+#define  VTK_TIME_INFO                  YES
+#define  GLM_EXTENDED                   YES
+#define  INITIAL_SMOOTHING              YES
+#define  CHAR_LIMITING                  YES
+#define  LIMITER                        MC_LIM
+#define  PRINT_TO_FILE                  NO
+#define  H_MASS_FRAC                    1.0
+#define  He_MASS_FRAC                   0.0
+#define  INTERNAL_BOUNDARY              YES
 
 /* user-defined code configuration labels (options) */
 #define  CH_OUTFLOW                     0
@@ -64,7 +63,7 @@
 #define  INIT_DOMAIN_LOAD_HS_EQUIL      4
 
 /* user-defined code configuration labels (definitions) */
-#define  CH_BOUNDARY_PRS_RHO     CH_STRATIFICATION
-#define  CH_BOUNDARY_B           CH_EXTRAPOLATION
+#define  CH_BOUNDARY_PRS_RHO            CH_STRATIFICATION
+#define  CH_BOUNDARY_B                  CH_EXTRAPOLATION
 
 /* [End] user-defined constants (do not change this line) */
